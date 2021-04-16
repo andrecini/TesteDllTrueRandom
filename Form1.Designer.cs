9791a942e2de94044d6353d0414af7e47535e5e2
andrepresys <andre.presys@gmail.com>
2021-04-16 10:42:02 -0300

    Ainda não segue a curva normal

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace TesteDllTrueRandom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSortear = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbMinValue = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMaxValue = new System.Windows.Forms.TextBox();
+            this.txtMinValue = new System.Windows.Forms.TextBox();
+            this.txtQuantify = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(615, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.tabControl1.Location = new System.Drawing.Point(190, 25);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(425, 356);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.chart1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(417, 330);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Chart";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend4.Enabled = false;
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(3, 3);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(411, 324);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(417, 330);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Grid";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(411, 324);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSortear);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cbMinValue);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtMaxValue);
+            this.groupBox1.Controls.Add(this.txtMinValue);
+            this.groupBox1.Controls.Add(this.txtQuantify);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(0, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(184, 356);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Preenchimento de Dados";
+            // 
+            // btnSortear
+            // 
+            this.btnSortear.Location = new System.Drawing.Point(90, 172);
+            this.btnSortear.Name = "btnSortear";
+            this.btnSortear.Size = new System.Drawing.Size(75, 23);
+            this.btnSortear.TabIndex = 6;
+            this.btnSortear.Text = "Sortear";
+            this.btnSortear.UseVisualStyleBackColor = true;
+            this.btnSortear.Click += new System.EventHandler(this.btnSortear_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Valor Máximo:";
+            // 
+            // cbMinValue
+            // 
+            this.cbMinValue.AutoSize = true;
+            this.cbMinValue.Location = new System.Drawing.Point(9, 70);
+            this.cbMinValue.Name = "cbMinValue";
+            this.cbMinValue.Size = new System.Drawing.Size(91, 17);
+            this.cbMinValue.TabIndex = 4;
+            this.cbMinValue.Text = "Valor Mínimo:";
+            this.cbMinValue.UseVisualStyleBackColor = true;
+            this.cbMinValue.CheckedChanged += new System.EventHandler(this.cbMinValue_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Quantidade:";
+            // 
+            // txtMaxValue
+            // 
+            this.txtMaxValue.Location = new System.Drawing.Point(10, 134);
+            this.txtMaxValue.Name = "txtMaxValue";
+            this.txtMaxValue.Size = new System.Drawing.Size(155, 20);
+            this.txtMaxValue.TabIndex = 2;
+            this.txtMaxValue.Text = "1000";
+            // 
+            // txtMinValue
+            // 
+            this.txtMinValue.Enabled = false;
+            this.txtMinValue.Location = new System.Drawing.Point(10, 89);
+            this.txtMinValue.Name = "txtMinValue";
+            this.txtMinValue.Size = new System.Drawing.Size(155, 20);
+            this.txtMinValue.TabIndex = 1;
+            this.txtMinValue.Text = "100";
+            // 
+            // txtQuantify
+            // 
+            this.txtQuantify.Location = new System.Drawing.Point(10, 44);
+            this.txtQuantify.Name = "txtQuantify";
+            this.txtQuantify.Size = new System.Drawing.Size(155, 20);
+            this.txtQuantify.TabIndex = 0;
+            this.txtQuantify.Text = "1000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 381);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.toolStrip1);
+            this.MaximumSize = new System.Drawing.Size(631, 420);
+            this.MinimumSize = new System.Drawing.Size(631, 420);
+            this.Name = "Form1";
+            this.Text = "Teste Dll True Random";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSortear;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox cbMinValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMaxValue;
+        private System.Windows.Forms.TextBox txtMinValue;
+        private System.Windows.Forms.TextBox txtQuantify;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Presys.Random;
+
+namespace TesteDllTrueRandom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static TrueRandom rdm = new TrueRandom();
+
+        private void btnSortear_Click(object sender, EventArgs e)
+        {
+            BindingSource bsG = new BindingSource();
+            bsG.DataSource = ReturnDataTable(listTreatment(SortNumbers()));
+            dataGridView1.DataSource = bsG;
+
+            chart1.DataSource = dataGridView1.DataSource;
+
+            SetSeries();
+
+            chart1.DataBind();
+        }
+
+        private int[] GetData(int quantify, int min, int max)
+        {
+            int[] output = new int[3];
+
+            output[0] = quantify;
+            output[1] = min;
+            output[2] = max;
+
+            return output;
+        }
+
+        private int[] GetData(int quantify, int max)
+        {
+            int[] output = new int[2];
+
+            output[0] = quantify;
+            output[1] = max;
+
+            return output;
+        }
+
+        private List<long> SortNumbers()
+        {
+            if (cbMinValue.Checked)
+            {
+                int[] data = GetData(int.Parse(txtQuantify.Text), int.Parse(txtMinValue.Text), int.Parse(txtMaxValue.Text));
+                
+                return rdm.SetInteger(data[0], data[1], data[2]).ToList();
+            }
+            else
+            {
+                int[] data = GetData(int.Parse(txtQuantify.Text), int.Parse(txtMaxValue.Text));
+
+                return rdm.SetInteger(data[0], 0, data[1]).ToList();
+            }
+
+        }
+
+        private List<long> listTreatment(List<long> list)
+        {
+            long[] vector = new long[long.Parse(txtMaxValue.Text) + 1];
+
+            VectorTreatment(ref vector);
+
+            foreach (var item in list)
+            {
+                vector[item]++;
+            }
+
+            return vector.ToList();
+        }
+
+        private void VectorTreatment(ref long[] vector)
+        {
+            for (int i = 0; i < vector.Length; i++)
+            {
+                vector[i] = 0;
+            }
+        }
+
+        private DataTable ReturnDataTable(List<long> list)
+        {
+            DataTable dt = new DataTable();
+
+            dt.Columns.Add("Número");
+            dt.Columns.Add("Repetições");
+
+            SetDataTable(ref dt, list);
+
+            return dt;
+        }
+
+        private void SetDataTable(ref DataTable dados, List<long> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                dados.Rows.Add(i.ToString(), list[i]);
+            }
+        }
+
+        private void cbMinValue_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbMinValue.Checked)
+                txtMinValue.Enabled = true;
+            else
+                txtMinValue.Enabled = true;
+        }
+
+        private void SetSeries()
+        {
+            chart1.Series[0].LegendText = "Números Gerados";
+            chart1.Series[0].XValueMember = "Número";
+            chart1.Series[0].YValueMembers = "Repetições";
+        }
+    }
+}
